fd17f73d1a4da5677ad2418e877f60a16d475ad2
CesarIzquierdo <cesarizquierdo100@gmail.com>
2024-03-05 15:08:19 -0600

    pagina de dnd para las razas y mas cosas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { RazasComponent } from './razas/razas.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ErrorPersonalizadoComponent } from './error-personalizado/error-personalizado.component';
+
+
+const appRoutes:Routes =[
+  {path:'', component:HomeComponent},
+  {path:'razas', component: RazasComponent},
+  {path:'contacto', component:ContactoComponent},
+  {path:'**',component:ErrorPersonalizadoComponent},
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    RazasComponent,
+    ContactoComponent,
+    ErrorPersonalizadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,  FormsModule, RouterModule.forRoot(appRoutes), HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
